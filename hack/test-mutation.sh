cc580bfe2252db53fdbddfb61823be7df4b391e2
Michael Shitrit <mshitrit@redhat.com>
2021-08-01 11:38:10 +0300

    * Printing mutation tests output in case it failed
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- hack/test-mutation.sh
+++ hack/test-mutation.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 expectedScore=0.395
 echo "starting mutation tests expected score at least: ${expectedScore}"
-actualScore=$(go-mutesting controllers | grep "The mutation score is" | awk '{print $5}')
+testsOutput=$(go-mutesting controllers)
+actualScore=$(echo "$testsOutput" | grep "The mutation score is" | awk '{print $5}')
 if (( $(echo "$actualScore >= $expectedScore" |bc -l) )); then
   echo "mutation tests passed with score: ${actualScore}"
 	exit 0
 fi
+echo "$testsOutput"
 echo "mutation tests failed expectedScore: ${expectedScore} , actualScore: ${actualScore}"
 exit 1
