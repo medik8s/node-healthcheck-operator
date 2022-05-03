5ef8475a83697c02f8c78c831d352994d03588a0
Marc Sluiter <msluiter@redhat.com>
2022-05-03 16:04:50 +0200

    Removed in this stage unneeded code
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- controllers/machinehealthcheck_controller.go
+++ controllers/machinehealthcheck_controller.go
@@ -4,18 +4,17 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
-	"github.com/medik8s/node-healthcheck-operator/controllers/cluster"
-	"github.com/medik8s/node-healthcheck-operator/controllers/mhc"
-	"github.com/medik8s/node-healthcheck-operator/controllers/utils"
+
 	"github.com/openshift/api/machine/v1beta1"
-	v1 "k8s.io/api/core/v1"
+
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/medik8s/node-healthcheck-operator/controllers/cluster"
+	"github.com/medik8s/node-healthcheck-operator/controllers/mhc"
 )
 
 // MachineHealthCheckReconciler reconciles a MachineHealthCheck object
@@ -28,11 +27,7 @@ type MachineHealthCheckReconciler struct {
 	MHCChecker                  mhc.Checker
 }
 
-// +kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch
-// +kubebuilder:rbac:groups=config.openshift.io,resources=clusterversions,verbs=get;list;watch
-// +kubebuilder:rbac:groups=machine.openshift.io,resources=machinehealthchecks,verbs=get;list;watch;update;patch
-// +kubebuilder:rbac:groups=machine.openshift.io,resources=machinehealthchecks/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=machine.openshift.io,resources=machinehealthchecks/finalizers,verbs=update
+// +kubebuilder:rbac:groups=machine.openshift.io,resources=machinehealthchecks,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -64,6 +59,5 @@ func (r *MachineHealthCheckReconciler) Reconcile(ctx context.Context, req ctrl.R
 func (r *MachineHealthCheckReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1beta1.MachineHealthCheck{}).
-		Watches(&source.Kind{Type: &v1.Node{}}, handler.EnqueueRequestsFromMapFunc(utils.MHCByNodeMapperFunc(mgr.GetClient(), mgr.GetLogger()))).
 		Complete(r)
 }
--- controllers/utils/mapper.go
+++ controllers/utils/mapper.go
@@ -4,8 +4,7 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
-	remediationv1alpha1 "github.com/medik8s/node-healthcheck-operator/api/v1alpha1"
-	machinev1beta1 "github.com/openshift/api/machine/v1beta1"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
@@ -13,6 +12,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	remediationv1alpha1 "github.com/medik8s/node-healthcheck-operator/api/v1alpha1"
 )
 
 // NHCByNodeMapperFunc return the Node-to-NHC mapper function
@@ -38,34 +39,6 @@ func NHCByNodeMapperFunc(c client.Client, logger logr.Logger) handler.MapFunc {
 			err = c.Get(context.Background(), client.ObjectKey{Name: o.GetName()}, node)
 			if selector.Matches(labels.Set(node.GetLabels())) {
 				r = append(r, reconcile.Request{NamespacedName: types.NamespacedName{Name: nhc.GetName()}})
-			}
-		}
-		return r
-	}
-	return delegate
-}
-
-// MHCByNodeMapperFunc return the Node-to-MHC mapper function
-func MHCByNodeMapperFunc(c client.Client, logger logr.Logger) handler.MapFunc {
-	// Find all MHC which target the given Node
-	delegate := func(o client.Object) []reconcile.Request {
-		var mhcList machinev1beta1.MachineHealthCheckList
-		err := c.List(context.Background(), &mhcList, &client.ListOptions{})
-		if err != nil {
-			return nil
-		}
-		var r []reconcile.Request
-		for _, mhc := range mhcList.Items {
-			selector, err := metav1.LabelSelectorAsSelector(&mhc.Spec.Selector)
-			if err != nil {
-				logger.Error(err, "failed to use the MHC selector.")
-				continue
-			}
-
-			node := &v1.Node{}
-			err = c.Get(context.Background(), client.ObjectKey{Name: o.GetName()}, node)
-			if selector.Matches(labels.Set(node.GetLabels())) {
-				r = append(r, reconcile.Request{NamespacedName: types.NamespacedName{Namespace: mhc.GetNamespace(), Name: mhc.GetName()}})
 			}
 		}
 		return r
