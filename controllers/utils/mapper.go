0c2b901a3731971f16d102e074d7449dcaf88c4d
Marc Sluiter <msluiter@redhat.com>
2022-12-19 10:21:50 +0100

    Fix NHCByNode mapper
    - don't crash for invalid selectors
    - get node once only
    - align code style
    
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- controllers/utils/mapper.go
+++ controllers/utils/mapper.go
@@ -23,25 +23,32 @@ func NHCByNodeMapperFunc(c client.Client, logger logr.Logger) handler.MapFunc {
 	// are unrelated to this node. Its even possible that the node still doesn't
 	// have the right labels set to be picked up by the nhc selector.
 	delegate := func(o client.Object) []reconcile.Request {
-		var nhcList remediationv1alpha1.NodeHealthCheckList
-		err := c.List(context.Background(), &nhcList, &client.ListOptions{})
-		if err != nil {
-			return nil
+		requests := make([]reconcile.Request, 0)
+
+		node := &v1.Node{}
+		if err := c.Get(context.Background(), client.ObjectKey{Name: o.GetName()}, node); err != nil {
+			logger.Error(err, "failed to get node", "node name", o.GetName())
+			return requests
 		}
-		var r []reconcile.Request
+
+		nhcList := &remediationv1alpha1.NodeHealthCheckList{}
+		if err := c.List(context.Background(), nhcList, &client.ListOptions{}); err != nil {
+			logger.Error(err, "failed to list NHCs")
+			return requests
+		}
+
 		for _, nhc := range nhcList.Items {
 			selector, err := metav1.LabelSelectorAsSelector(&nhc.Spec.Selector)
 			if err != nil {
-				logger.Error(err, "failed to use the NHC selector.")
+				logger.Error(err, "invalid node selector", "NHC name", nhc.GetName())
+				continue
 			}
 
-			node := &v1.Node{}
-			err = c.Get(context.Background(), client.ObjectKey{Name: o.GetName()}, node)
 			if selector.Matches(labels.Set(node.GetLabels())) {
-				r = append(r, reconcile.Request{NamespacedName: types.NamespacedName{Name: nhc.GetName()}})
+				requests = append(requests, reconcile.Request{NamespacedName: types.NamespacedName{Name: nhc.GetName()}})
 			}
 		}
-		return r
+		return requests
 	}
 	return delegate
 }
