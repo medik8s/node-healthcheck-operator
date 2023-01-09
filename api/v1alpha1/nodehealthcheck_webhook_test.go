d842a9cf6471dca00e28c5355251c966d64559aa
Marc Sluiter <msluiter@redhat.com>
2023-01-09 16:15:14 +0100

    Prevent template update during remediations
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- api/v1alpha1/nodehealthcheck_webhook_test.go
+++ api/v1alpha1/nodehealthcheck_webhook_test.go
@@ -4,13 +4,14 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 var _ = Describe("NodeHealthCheck Validation", func() {
 
-	Context("a NHC", func() {
+	Context("Creating or updating a NHC", func() {
 
 		var nhc *NodeHealthCheck
 
@@ -71,4 +72,67 @@ var _ = Describe("NodeHealthCheck Validation", func() {
 		})
 	})
 
+	Context("During ongoing remediation", func() {
+
+		var nhcOld *NodeHealthCheck
+		var nhcNew *NodeHealthCheck
+
+		BeforeEach(func() {
+			mh := intstr.FromString("51%")
+			nhcOld = &NodeHealthCheck{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: NodeHealthCheckSpec{
+					MinHealthy: &mh,
+					Selector: metav1.LabelSelector{
+						MatchExpressions: []metav1.LabelSelectorRequirement{
+							{
+								Key:      "node-role.kubernetes.io/control-plane",
+								Operator: metav1.LabelSelectorOpDoesNotExist,
+							},
+						},
+					},
+					RemediationTemplate: &v1.ObjectReference{
+						Name: "OldName",
+					},
+				},
+				Status: NodeHealthCheckStatus{
+					InFlightRemediations: map[string]metav1.Time{
+						"test": metav1.Now(),
+					},
+				},
+			}
+		})
+
+		Context("updating selector", func() {
+			BeforeEach(func() {
+				nhcNew = nhcOld.DeepCopy()
+				nhcNew.Spec.Selector.MatchExpressions[0].Key = "node-role.kubernetes.io/infra"
+			})
+			It("should be denied", func() {
+				Expect(nhcNew.ValidateUpdate(nhcOld)).To(MatchError(
+					And(
+						ContainSubstring(OngoingRemediationError),
+						ContainSubstring("selector"),
+					),
+				))
+			})
+		})
+
+		Context("updating template", func() {
+			BeforeEach(func() {
+				nhcNew = nhcOld.DeepCopy()
+				nhcNew.Spec.RemediationTemplate.Name = "newName"
+			})
+			It("should be denied", func() {
+				Expect(nhcNew.ValidateUpdate(nhcOld)).To(MatchError(
+					And(
+						ContainSubstring(OngoingRemediationError),
+						ContainSubstring("remediation template"),
+					),
+				))
+			})
+		})
+	})
 })
