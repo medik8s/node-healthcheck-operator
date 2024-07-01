8fc6d4f4987cbf46f015bc78dd3b45a714bdc37d
Marc Sluiter <msluiter@redhat.com>
2024-07-01 15:09:40 +0200

    Fix CR exists check in e2e test
    CR doesn't always have node name as name anymore
    
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- e2e/common_test.go
+++ e2e/common_test.go
@@ -2,6 +2,9 @@ package e2e
 
 import (
 	"context"
+	"fmt"
+
+	commonannotations "github.com/medik8s/common/pkg/annotations"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -10,7 +13,9 @@ import (
 
 func ensureRemediationResourceExists(name string, namespace string, remediationResource schema.GroupVersionResource) func() error {
 	return func() error {
-		_, err := dynamicClient.Resource(remediationResource).Namespace(namespace).Get(context.Background(), name, metav1.GetOptions{})
+		// The CR name doesn't always match the node name in case multiple CRs of same type for the same node are supported
+		// So list all, and look for the node name in the annotation
+		list, err := dynamicClient.Resource(remediationResource).Namespace(namespace).List(context.Background(), metav1.ListOptions{})
 		if err != nil {
 			if errors.IsNotFound(err) {
 				log.Info("didn't find remediation resource yet")
@@ -19,7 +24,13 @@ func ensureRemediationResourceExists(name string, namespace string, remediationR
 			}
 			return err
 		}
-		log.Info("found remediation resource")
-		return nil
+		for _, cr := range list.Items {
+			if nodeName, exists := cr.GetAnnotations()[commonannotations.NodeNameAnnotation]; exists && nodeName == name {
+				log.Info("found remediation resource")
+				return nil
+			}
+		}
+		log.Info("didn't find remediation resource yet")
+		return fmt.Errorf("not found")
 	}
 }
