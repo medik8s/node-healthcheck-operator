a159783505d713f6c68183d7ead865d15c5b5e2b
Marc Sluiter <msluiter@redhat.com>
2024-12-05 17:20:37 +0100

    Check control plane topology
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- controllers/cluster/capabilities.go
+++ controllers/cluster/capabilities.go
@@ -1,16 +1,23 @@
 package cluster
 
 import (
+	"context"
+
+	"github.com/go-logr/logr"
+
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	configv1 "github.com/openshift/api/config/v1"
 )
 
 type Capabilities struct {
-	IsOnOpenshift, HasMachineAPI bool
+	IsOnOpenshift, HasMachineAPI, IsSupportedControlPlaneTopology bool
 }
 
-func NewCapabilities(config *rest.Config) (Capabilities, error) {
+func NewCapabilities(config *rest.Config, reader client.Reader, logger logr.Logger, ctx context.Context) (Capabilities, error) {
 	dc, err := discovery.NewDiscoveryClientForConfig(config)
 	if err != nil {
 		return Capabilities{}, err
@@ -25,6 +32,8 @@ func NewCapabilities(config *rest.Config) (Capabilities, error) {
 	machineAPIGroup := schema.GroupVersion{Group: "machine.openshift.io", Version: "v1"}
 
 	caps := Capabilities{}
+
+	// check API groups for OCP and MachineAPI groups
 	for _, apiGroup := range apiGroups.Groups {
 		for _, supportedVersion := range apiGroup.Versions {
 			if supportedVersion.GroupVersion == ocpKind.GroupVersion().String() {
@@ -38,5 +47,32 @@ func NewCapabilities(config *rest.Config) (Capabilities, error) {
 			}
 		}
 	}
+
+	// check topology, default to true on non-openshift clusters
+	caps.IsSupportedControlPlaneTopology = true
+	if caps.IsOnOpenshift {
+		infra := &configv1.Infrastructure{}
+		err = reader.Get(ctx, client.ObjectKey{Name: "cluster"}, infra)
+		if err != nil {
+			return Capabilities{}, err
+		}
+		cpTopology := infra.Status.ControlPlaneTopology
+
+		// add new supported topology modes here if needed
+		// don't allow new topologies by default, because they might implement their own fencing logic, which can conflict with this operator
+		// see https://github.com/openshift/api/blob/c1fdeb0788c16659238d93ec45ce2e798c14eb88/config/v1/types_infrastructure.go#L129-L147
+		if cpTopology == configv1.HighlyAvailableTopologyMode ||
+			cpTopology == configv1.SingleReplicaTopologyMode ||
+			cpTopology == configv1.ExternalTopologyMode {
+
+			logger.Info("supported control plane topology", "topology", cpTopology)
+			caps.IsSupportedControlPlaneTopology = true
+
+		} else {
+			logger.Info("unsupported control plane topology", "topology", cpTopology)
+			caps.IsSupportedControlPlaneTopology = false
+		}
+	}
+
 	return caps, nil
 }
